c5291ed72604eb48bf9cf65f73b2a0a841f3e08d
KarimBaggarii <karimbaggarrii@gmail.com>
2021-07-21 06:07:33 +0100

    vue added & components & views

--- BalaB/resources/js/app.js
+++ BalaB/resources/js/app.js
@@ -1,1 +1,10 @@
-require('./bootstrap');
+require("./bootstrap");
+
+window.Vue = require("vue").default;
+import Heading from './components/Heading.vue';
+const app = new Vue({
+    el: "#app",
+    components:{
+        Heading
+    }
+});
---
+++ BalaB/resources/js/components/Heading.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+      <h1>myheader broski</h1>
+  </div>
+</template>
